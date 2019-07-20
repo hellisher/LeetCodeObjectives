56414911e2389529f162d77128e887c4023e2da1
Valerii El-Khatib <elkhatibvm@gmail.com>
2019-07-20 16:31:26 +0300

    Added two sum function

--- LeetCodeObjectives.playground/Contents.swift
+++ LeetCodeObjectives.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
-var str = "Hello, playground"
+
+//Date 20/07/2019
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var twoIndex = [Int]()
+    var indexOne: Int = 0
+    while indexOne < nums.count {
+        var indexTwo: Int = indexOne + 1
+        while indexTwo < nums.count {
+            if (nums[indexTwo] == target - nums[indexOne]) {
+                twoIndex = [indexOne, indexTwo]
+            }
+            indexTwo += 1
+        }
+        indexOne += 1
+    }
+    var answer = [Int]()
+    if twoIndex.count == 2 {
+        answer = twoIndex
+        print(answer)
+    } else {
+        print("No two sum solution")
+    }
+    return answer
+}
+
+var array = [2, 10, 2, 3, 1, 56]
+var targetOne = 7
+var test = twoSum(array, targetOne)
